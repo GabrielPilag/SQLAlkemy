0acbc00412b22d0b4135d9610db2a9d4749f415d
Gabriel Pilagatto <gabrielpilagatto@gmail.com>
2022-01-06 19:45:12 -0300

    first commit

---
+++ SQLite.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `curso` ( `codigo` INT NOT NULL , `nombre` VARCHAR(255) NOT NULL , `descripcion` VARCHAR(255) NOT NULL , `turno` VARCHAR(255) NOT NULL , PRIMARY KEY (`codigo`));
+ALTER TABLE `curso` ADD `cupo` INT NOT NULL;
+INSERT INTO `curso` (`codigo`, `nombre`, `descripcion`, `turno`, `cupo`) VALUES ('101', 'Algoritmos', 'Algoritmos y Estructuras de Datos', 'Mañana', '35'), ('102', 'Matemática Discreta', '', 'Tarde', '30');
+INSERT INTO `curso` (`codigo`, `descripcion`, `turno`, `cupo`) VALUES ('101', 'Algoritmos y Estructuras de Datos', 'Mañana', '35');
+INSERT INTO `curso` (`codigo`, `nombre`, `descripcion`, `turno`, `cupo`) VALUES ('101', 'Algoritmos', 'Algoritmos y Estructuras de Datos', 'Mañana', '35');
+UPDATE `curso` SET  `cupo`= '25';
+DELETE FROM `curso` WHERE `curso`.`codigo` = 101;
